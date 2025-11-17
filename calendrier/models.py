eef3ac4e5458d0cad609f8019359d05b63c2e833
Rollanda437 <zossourolande93@gmail.com>
2025-11-17 15:11:51 +0100

    Fix final: static files + warning calendrier

--- calendrier/models.py
+++ calendrier/models.py
@@ -14,4 +14,4 @@ class Evenement(models.Model):
     type_evenement = models.CharField(max_length=20, choices=TYPE_CHOICE)
     
     def __str__(self):
-        return f"{self.titre}"({self.type_evenement})
+        return f"{self.titre} ({self.type_evenement})"
