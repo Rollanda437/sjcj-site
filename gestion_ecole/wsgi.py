70e8a910b684f3c5da2a237d677f1da751d1a9ed
Rollanda437 <zossourolande93@gmail.com>
2025-11-17 15:05:54 +0100

    Fix Vercel: variable app manquante

--- gestion_ecole/wsgi.py
+++ gestion_ecole/wsgi.py
@@ -1,5 +1,12 @@
+
+"""
+WSGI config for gestion_ecole project.
+Vercel exige que la variable s'appelle exactement « app »
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_ecole.settings')
-application = get_wsgi_application()
+
+app = get_wsgi_application()
